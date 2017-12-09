67ac718b31c46feab0c150cfdc96bbd31b351f82
McWenker <nlax76@gmail.com>
2017-12-09 17:23:02 -0500

    december 9 2017 update

---
+++ Assets/QuestPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestPickup : MonoBehaviour
+{
+    [SerializeField]
+    ItemScriptableObject itemData;
+
+    public ItemScriptableObject ItemData
+    {
+        get
+        {
+            return itemData;
+        }
+    }
+}
